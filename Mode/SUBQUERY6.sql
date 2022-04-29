16a667bb23292132af9848703034cd11e81f839f
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-29 17:50:14 -0500

    Create SUBQUERY6.sql

---
+++ Mode/SUBQUERY6.sql
@@ -0,0 +1,13 @@
+SELECT incidents.*,
+       sub.incidents AS incidents_that_day
+  FROM tutorial.sf_crime_incidents_2014_01 incidents
+  JOIN ( SELECT date,
+          COUNT(incidnt_num) AS incidents
+           FROM tutorial.sf_crime_incidents_2014_01
+          GROUP BY 1
+       ) sub
+    ON incidents.date = sub.date
+ ORDER BY sub.incidents DESC, time
+ 
+ 
+ -- The following query ranks all of the results according to how many incidents were reported in a given day.
