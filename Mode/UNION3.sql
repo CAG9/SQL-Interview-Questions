302a3f0ab21bc679ed8ff759fc68a8de1a6b879a
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-29 18:50:32 -0500

    Create UNION3.sql

---
+++ Mode/UNION3.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(*) AS total_rows
+  FROM (
+        SELECT *
+          FROM tutorial.crunchbase_investments_part1
+
+         UNION ALL
+
+        SELECT *
+          FROM tutorial.crunchbase_investments_part2
+       ) sub;
