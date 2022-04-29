19c3394ec1eb595fb02d52482b2d87bb7f14be85
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-28 21:46:26 -0500

    Create JOINCOMPARISON.sql

---
+++ Mode/JOINCOMPARISON.sql
@@ -0,0 +1,9 @@
+SELECT companies.permalink,
+       companies.name,
+       companies.status,
+       COUNT(investments.investor_permalink) AS investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments_part1 investments
+    ON companies.permalink = investments.company_permalink
+   AND investments.funded_year > companies.founded_year + 5
+ GROUP BY 1,2, 3;
