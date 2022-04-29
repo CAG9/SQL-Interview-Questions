cae776fc191e98796f83126426416da16f9c8702
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-28 21:32:51 -0500

    Create UNION1.sql

---
+++ Mode/UNION1.sql
@@ -0,0 +1,9 @@
+SELECT company_permalink, company_name,  investor_name 
+FROM tutorial.crunchbase_investments_part1
+WHERE  company_name ILIKE 'T%'
+
+UNION ALL 
+
+SELECT company_permalink, company_name,  investor_name 
+FROM tutorial.crunchbase_investments_part1
+WHERE  company_name ILIKE 'M%';
