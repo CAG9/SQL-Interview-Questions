ffc10b46329a3843bbf43ba0c43f56ac5399de21
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-29 17:47:45 -0500

    Create SUBQUERY4.sql

---
+++ Mode/SUBQUERY4.sql
@@ -0,0 +1,7 @@
+SELECT *
+  FROM tutorial.sf_crime_incidents_2014_01
+ WHERE Date IN (SELECT date
+                 FROM tutorial.sf_crime_incidents_2014_01
+                ORDER BY date
+                LIMIT 5
+              );
