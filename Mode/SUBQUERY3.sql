183a6dea04e1196020560471a6c26b7183726869
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-29 17:39:35 -0500

    Create SUBQUERY3.sql

---
+++ Mode/SUBQUERY3.sql
@@ -0,0 +1,10 @@
+SELECT sub.category,
+       AVG(sub.incidents) AS avg_incidents_per_month
+  FROM (
+        SELECT EXTRACT('month' FROM cleaned_date) AS month,
+               category,
+               COUNT(1) AS incidents
+          FROM tutorial.sf_crime_incidents_cleandate
+         GROUP BY 1,2
+       ) sub
+ GROUP BY 1;
