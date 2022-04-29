d043da7eb5c8a759253f83b436d91032174dd4b3
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-04-29 17:25:58 -0500

    Create SUBQUERY2.sql

---
+++ Mode/SUBQUERY2.sql
@@ -0,0 +1,13 @@
+-- what if you wanted to know how many incidents happen, on average, on a Friday in December? In January? 
+SELECT LEFT(sub.date, 2) AS cleaned_month,
+       sub.day_of_week,
+       AVG(sub.incidents) AS average_incidents
+  FROM (
+        SELECT day_of_week,
+               date,
+               COUNT(incidnt_num) AS incidents
+          FROM tutorial.sf_crime_incidents_2014_01
+         GROUP BY 1,2
+       ) sub
+ GROUP BY 1,2
+ ORDER BY 1,2
