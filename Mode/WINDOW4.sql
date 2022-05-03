aad281e97258ca8455f112c04373defa20b3d273
Cesar Arcos <60860385+CAG9@users.noreply.github.com>
2022-05-03 12:41:06 -0500

    Create WINDOW4.sql

---
+++ Mode/WINDOW4.sql
@@ -0,0 +1,7 @@
+SELECT end_terminal,
+       duration_seconds,
+       SUM(duration_seconds) OVER
+         (PARTITION BY end_terminal ORDER BY duration_seconds DESC)
+         AS running_total
+  FROM tutorial.dc_bikeshare_q1_2012
+ WHERE start_time < '2012-01-08';
